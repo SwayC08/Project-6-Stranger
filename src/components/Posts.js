f478305c17984654d1ffc316ecd2faa20d2c2ed9
Josue <ijosue.carreon@gmail.com>
2023-02-27 17:45:00 -0600

    update

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,22 @@
-const Post = () => {
+import { Link } from 'react-router-dom';
+
+const Post = (props) => {
+
     return(
-        <div>Post</div>
+        <div>
+            <h3></h3>
+            <div>
+                {
+                    props.posts.length ? props.posts.map((onePost) => {
+                        return (
+                            <div key={onePost._id}>
+                                <Link to={`/${onePost._id}`}> POST: { onePost.title }</Link>
+                            </div>
+                        )
+                    }) : <div>No Data Available</div>
+                }
+            </div>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,23 +4,49 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Home, Profile, Posts, Login, Logout, CreateNewPost, Header, ViewPost, Navbar } from "./components";
 
 const Main = ()=> {
+    const [posts, setPosts] = useState([]);
 
+    const COHORT_NAME ='2301-FTB-MT-WEB-FT';
+    const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+    // `${BASE_URL}/posts`
+
+    async function fetchPosts(){        
+        try{
+            // const response = await fetch("https://strangers-things.herokuapp.com/api/2301-FTB-MT-WEB-FT/posts");
+            // const transData = await response.json();
+            const returned = await fetch(`${BASE_URL}/posts`);
+            const transData = await returned.json();
+            setPosts(transData.data.posts);
+        } catch (error){
+            console.log(error)
+        }
+    }
+
+    useEffect(()=>{
+        fetchPosts();
+    }, [])
+    
     return(
         <BrowserRouter>
-        <Header/>
-        <Routes>
-            <Route path ="/Login" element={ <Login/> }/>
-            <Route path ="/" element={ <Home/> }/>
-            <Route path ="/Profile" element={ <Profile/> }/>
-            <Route path ="/Posts" element={ <Posts/> }/>
-            <Route path ="/CreateNewPost" element={ <CreateNewPost/> }/>
-            <Route path ="/Logout" element={ <Logout/> }/>
-            <Route path ="/Navbar" element={ <Navbar/> }/>
-        </Routes>
-    </BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path ="/Login" element={ <Login/> }/>
+                <Route path ="/" element={ <Home posts={ posts }/> }/>
+                <Route path ="/Profile" element={ <Profile posts={ posts }/> }/>
+                <Route path ="/Posts" element={ <Posts posts={ posts }/> }/>
+
+                <Route path ="/Posts/:_id" element={ <ViewPost posts={ posts } setPosts={ setPosts } /> }/>
+
+                <Route path ="/CreateNewPost" element={ <CreateNewPost posts={ posts }/> }/>
+                <Route path ="/Logout" element={ <Logout/> }/>
+                <Route path ="/Navbar" element={ <Navbar/> }/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
-const app = document.getElementById("app");
-let root = createRoot(app);
-root.render(<Main />)
+// const app = document.getElementById("app");
+// let root = createRoot(app);
+// root.render(<Main />)
+
+createRoot(document.getElementById("app")).render(< Main />)
