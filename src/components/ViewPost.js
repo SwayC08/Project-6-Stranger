2cd9cf296f141c39e0546f6fdac35b5faf0972bf
Josue <ijosue.carreon@gmail.com>
2023-02-27 21:13:35 -0600

    Update ViewPost.js

--- src/components/ViewPost.js
+++ src/components/ViewPost.js
@@ -1,22 +1,43 @@
-import { useParams, useNavigate } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 const ViewPost = (props) => {
 
+    const [editStat, setEditStat] = useState(false);
+
     const { _id } = useParams();
-    console.log(useParams())
+    console.log(useParams());
 
     let filterPosts;
 
     filterPosts = props.posts.filter((onePost) => {
         return onePost._id == _id
-    })
-    console.log(filterPosts)
+    });
+    console.log(filterPosts);
 
+    const [newPostNam , setNewPostNam] = useState(
+        filterPosts.length ? filterPosts[0].title : ""
+    );
 
+    const [newPostDesc, setNewPostDesc] = useState(
+        filterPosts.length ? filterPosts[0].description : ""
+    );
+
+    function togEditFrmFnc (){
+        setEditStat(!editStat)
+    };
+
+    const nav = useNavigate ();
+
+    async function putReqUpdateFnc (event){
+        event.preventDefault();
+        try {
+            const response = await fetch(`${BASE_URL}/posts`) 
+        }
+    };
 
     return(
-        <div>ViewPost
+        <div>
             <h3>Post Details {filterPosts[0]._id} </h3>
             {
                 filterPosts.length ? (
